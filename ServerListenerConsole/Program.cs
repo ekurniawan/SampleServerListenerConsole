2b2c168f606f2bfe378c95a63b2966191af906ce
ekurniawan <erick.kurniawan@gmail.com>
2018-12-30 19:05:07 +0700

    Add project files.

---
+++ ServerListenerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerListenerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
